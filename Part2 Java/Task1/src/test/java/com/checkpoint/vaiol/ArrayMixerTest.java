1c7f4083ebf3bdb82218c2044b259c9c914b1b12
Alexander <vaiol.ans@gmail.com>
2015-03-29 12:26:44 +0200

    Task1 92%

--- Part2 Java/Task1/src/test/java/com/checkpoint/vaiol/ArrayMixerTest.java
+++ Part2 Java/Task1/src/test/java/com/checkpoint/vaiol/ArrayMixerTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class ArrayMixerTest {
     private ArrayMixer arrayMixer;
-    private double percentage;
+    private static final double PERCENTAGE = 0.25;
     private int[] testArray1;
     private int[] testArray2;
     private int[] testArray3;
@@ -18,30 +18,36 @@ public class ArrayMixerTest {
     private int[][] testArray6;
     private int[][] testArray7;
     private int[][] testArray8;
+    private int[][] testArray9;
+    private int[] testArray10;
 
     public ArrayMixerTest() {
     }
 
     @Before
     public void setUpCalcTest() {
-        this.percentage = 0.25;
-        this.arrayMixer = new ArrayMixer((int)(this.percentage * 100.0D));
-        this.testArray1 = new int[10];
-        this.testArray2 = new int[100];
-        this.testArray3 = new int[234];
-        this.testArray4 = new int[54];
-        this.testArray5 = new int[88][20];
-        this.testArray6 = new int[10][43];
-        this.testArray7 = new int[234][2];
-        this.testArray8 = new int[54][24];
-        this.testArray1 = this.fillArray((int[])this.testArray1);
-        this.testArray2 = this.fillArray((int[])this.testArray2);
-        this.testArray3 = this.fillArray((int[])this.testArray3);
-        this.testArray4 = this.fillArray((int[])this.testArray4);
-        this.testArray5 = this.fillArray((int[][])this.testArray5);
-        this.testArray6 = this.fillArray((int[][])this.testArray6);
-        this.testArray7 = this.fillArray((int[][])this.testArray7);
-        this.testArray8 = this.fillArray((int[][])this.testArray8);
+        arrayMixer = new ArrayMixer((int)(PERCENTAGE * 100D));
+        testArray1 = new int[10];
+        testArray2 = new int[100];
+        testArray3 = new int[234];
+        testArray4 = new int[54];
+        testArray5 = new int[88][20];
+        testArray6 = new int[10][43];
+        testArray7 = new int[234][2];
+        testArray8 = new int[54][24];
+        testArray9 = new int[7][1];
+        testArray10 = new int[5];
+
+        testArray1 = fillArray(testArray1);
+        testArray2 = fillArray(testArray2);
+        testArray3 = fillArray(testArray3);
+        testArray4 = fillArray(testArray4);
+        testArray5 = fillArray(testArray5);
+        testArray6 = fillArray(testArray6);
+        testArray7 = fillArray(testArray7);
+        testArray8 = fillArray(testArray8);
+        testArray9 = fillArray(testArray9);
+        testArray10 = fillArray(testArray10);
     }
 
     private int[] fillArray(int[] array) {
@@ -66,42 +72,69 @@ public class ArrayMixerTest {
     @Test
     public void testMixer() {
 
-        int numberOfElement = (int)(testArray1.length * percentage);
+        /*
+        ВНИМАНИЕ!!!
+        При подсчете необходимого количества элементов для перемешивания (в зависисмости от %)
+        итоговове значение всегда округляется до меньшего целого числа.
+        Например: 25% от 10 => 2.5. А это дробное значение округляется до 2.
+         */
+        int numberOfElement = (int)(testArray1.length * PERCENTAGE);
         int[] mixedArray = arrayMixer.shuffle(testArray1);
         assertEquals(numberOfElement, countDifferentElement(testArray1, mixedArray));
 
-        numberOfElement = (int)(testArray2.length * percentage);
+        numberOfElement = (int)(testArray2.length * PERCENTAGE);
         mixedArray = arrayMixer.shuffle(testArray2);
         assertEquals(numberOfElement, countDifferentElement(testArray2, mixedArray));
 
-        numberOfElement = (int)(testArray2.length * percentage);
+        numberOfElement = (int)(testArray2.length * PERCENTAGE);
         mixedArray = arrayMixer.shuffle(testArray2);
         assertEquals(numberOfElement, countDifferentElement(testArray2, mixedArray));
 
-        numberOfElement = (int)(testArray3.length * percentage);
+        numberOfElement = (int)(testArray3.length * PERCENTAGE);
         mixedArray = arrayMixer.shuffle(testArray3);
         assertEquals(numberOfElement, countDifferentElement(testArray3, mixedArray));
 
-        numberOfElement = (int)(testArray4.length * percentage);
+        numberOfElement = (int)(testArray4.length * PERCENTAGE);
         mixedArray = arrayMixer.shuffle(testArray4);
         assertEquals(numberOfElement, countDifferentElement(testArray4, mixedArray));
 
 
-        numberOfElement = (int)(testArray5.length * testArray5[0].length  * percentage);
+        numberOfElement = (int)(testArray5.length * testArray5[0].length  * PERCENTAGE);
         int[][] mixedArrayTwoD = arrayMixer.shuffle(testArray5);
         assertEquals(numberOfElement, countDifferentElement(testArray5, mixedArrayTwoD));
 
-        numberOfElement = (int)(testArray6.length * testArray6[0].length  * percentage);
+        numberOfElement = (int)(testArray6.length * testArray6[0].length  * PERCENTAGE);
         mixedArrayTwoD = arrayMixer.shuffle(testArray6);
         assertEquals(numberOfElement, countDifferentElement(testArray6, mixedArrayTwoD));
 
-        numberOfElement = (int)(testArray7.length * testArray7[0].length  * percentage);
+        numberOfElement = (int)(testArray7.length * testArray7[0].length  * PERCENTAGE);
         mixedArrayTwoD = arrayMixer.shuffle(testArray7);
         assertEquals(numberOfElement, countDifferentElement(testArray7, mixedArrayTwoD));
 
-        numberOfElement = (int)(testArray8.length * testArray8[0].length  * percentage);
+        numberOfElement = (int)(testArray8.length * testArray8[0].length  * PERCENTAGE);
         mixedArrayTwoD = arrayMixer.shuffle(testArray8);
         assertEquals(numberOfElement, countDifferentElement(testArray8, mixedArrayTwoD));
+    }
+
+    @Test
+    public void testNoMix() {
+        /*
+        ВНИМАНИЕ!!!
+        Если количество элементов для перемешивания будет меньше чем 2.
+        То перестановок не будет вовсе. Так как невозможно сделать так,
+        чтобы только лишь 1 элемент в массиве поменял свое местоположение.
+        Был вариант, округлять до большего значения, например: 25% от 5 => 1.25,
+        округляем до 2 и перемешиваем, но в данном случае перемешается 40% массива,
+        а что делать если длина массива и вовсе = 2?.
+        Был еще один вариант, кидать исключение если перемешивание не удалось,
+        но автору показалось это плохой идеей.
+         */
+        int numberOfElement = 0;
+        int[][] mixedArray = arrayMixer.shuffle(testArray9);
+        assertEquals(numberOfElement, countDifferentElement(testArray9, mixedArray));
+
+        int[] mixArray = arrayMixer.shuffle(testArray10);
+        assertEquals(numberOfElement, countDifferentElement(testArray10, mixArray));
     }
 
     private int countDifferentElement(int[] array1, int[] array2) {
