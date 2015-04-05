9285cd5e4dcfee884f26d0ece9a94d7f283acb47
Alexander <vaiol.ans@gmail.com>
2015-04-05 22:07:38 +0300

    Task 2 init

---
+++ Part2 Java/Task2/src/test/java/com/checkpoint/vaiol/MainTest.java
@@ -0,0 +1,29 @@
+package com.checkpoint.vaiol;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MainTest {
+
+    @Before
+    public void setUpCalcTest() {
+
+    }
+
+    @After
+    public void tearDownCalcTest() {
+
+    }
+
+
+    @Test
+    public void testCalcAndThreads() {
+        assertEquals(5, 5);
+    }
+
+}
