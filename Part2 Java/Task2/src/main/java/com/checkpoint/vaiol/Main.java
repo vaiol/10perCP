47b863cee85c6876530dfed84cf414fd72a739cb
Alexander <vaiol.ans@gmail.com>
2015-04-06 04:53:14 +0300

    Task 2 12%

--- Part2 Java/Task2/src/main/java/com/checkpoint/vaiol/Main.java
+++ Part2 Java/Task2/src/main/java/com/checkpoint/vaiol/Main.java
@@ -1,7 +1,15 @@
 package com.checkpoint.vaiol;
 
+import com.checkpoint.vaiol.myDataStructure.MyArrayList;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        MyArrayList<Integer> name = new MyArrayList<Integer>();
+        System.out.println(name.size());
+        name.add(0, 1);
+        System.out.println(name.get(0));
     }
 }
--- Part2 Java/Task2/src/main/java/com/checkpoint/vaiol/myDataStructure/MyArrayList.java
+++ Part2 Java/Task2/src/main/java/com/checkpoint/vaiol/myDataStructure/MyArrayList.java
@@ -1,19 +1,55 @@
 package com.checkpoint.vaiol.myDataStructure;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MyArrayList<E> implements List<E> {
+
+
+    private Entity<E>[] array;
+    private int currentSize;
+
+    private static final int INITIAL_CAPACITY = 10;
+    private static final double FACTOR = 1.5;
+
+    public MyArrayList() {
+        currentSize = 0;
+        array = new Entity[INITIAL_CAPACITY];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        currentSize = 0;
+        array = new Entity[initialCapacity];
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if(index > currentSize || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if(currentSize >= array.length) {
+            Entity[] newArray = new Entity[ (int)(array.length * FACTOR) ];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+        System.arraycopy(array, index, array, index + 1, array.length - (index + 1));
+        array[index] = new Entity<E>(element, System.currentTimeMillis());
+        currentSize++;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return currentSize;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return currentSize == 0;
     }
 
     @Override
@@ -36,10 +72,7 @@ public class MyArrayList<E> implements List<E> {
         return null;
     }
 
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
+
 
     @Override
     public boolean remove(Object o) {
@@ -73,27 +106,34 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        array = new Entity[INITIAL_CAPACITY];
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return array[index].object;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        if(index > currentSize || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        E tmp = array[index].object;
+        array[index] = new Entity<E>(element, System.currentTimeMillis());
+        return tmp;
     }
 
-    @Override
-    public void add(int index, E element) {
 
-    }
 
     @Override
     public E remove(int index) {
-        return null;
+        if(index > currentSize || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        E tmp = array[index].object;
+        System.arraycopy(array, index + 1, array, index, array.length - (index + 1));
+        return tmp;
     }
 
     @Override
@@ -119,5 +159,14 @@ public class MyArrayList<E> implements List<E> {
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         return null;
+    }
+
+    private class Entity<E> {
+        public Entity(E object, long createTime) {
+            this.object = object;
+            this.createTime = createTime;
+        }
+        private E object;
+        private long createTime;
     }
 }
